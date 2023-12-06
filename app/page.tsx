06dcf85e14c744c76ceed22ddba12755148205e7
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-06 19:57:02 +0200

    added the play page...

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-screen align-middle flex-col items-center justify-between p-24 bg-[#333333]">
-      
+    <main className="flex h-screen align-middle flex-col items-center justify-center p-24 bg-[#333333]">
+      <div className=''>
         <h3 className='text-center font-extrabold align-middle text-8xl'>Triad</h3>
+        <p>Triad provides the best engaging games on the internet to help you relax and smile a little.</p>
+      </div>
+
+      <button className='border-2 borcer-white p-4 mt-8'>Play Now !</button>
       
     </main>
   )
---
+++ app/play/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Play = () => {
+  return (
+    <main className="p-24 h-screen align-middle   bg-[#333333]">
+    <div className=''>
+        <h3 className='underline font-bold text-2xl'>Steps</h3>
+        <div className='m-4'>
+            <ol className='ml-6 list-decimal'  >
+                <li>Download</li>
+                <li>play</li>
+                <li>Rank</li>
+            </ol>
+        </div>
+        <h3 className='underline font-bold text-2xl'>Our popular Games</h3>
+    </div>
+    
+  </main>
+  )
+}
+
+export default Play
